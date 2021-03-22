4694652ee318b66f0bbef76e78f850fe0e37fb20
Katie <kassapides@gmail.com>
2021-03-22 16:44:57 -0700

    adding my project to gitHub

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+
+
+
+console.log('javascript loaded');
+
+function sayHi() {
+    alert("hi");
+    alert("Warning! your computer will self-destruct in");
+    // 2. Display a "3... 2... 1..." countdown using 3 alert dialog boxes
+    alert("3");
+    alert("2");
+    alert("1");
+    alert("are you running?");
+    alert("you should be very very very . . .");
+    alert("very very very very very very very very very!");
+    alert("very very very terrified right now!!");
+    alert("Are you running away yet?");
+    alert("Nope, you're not running because you just click on me");
+    alert("I'll give you one more chance");
+    alert("3");
+    alert("2");
+    alert("1");
+    alert("0");
+    
+    // 3. This statement selects the <h1> element and replaces its text with "BOOM!".
+    document.querySelector("h1").textContent = "🔥KA BOOM! BING BLAM BOOOOOOOOM!🔥";
+    
+    // 4. Log "Message destroyed!" to the console
+    
+    alert("your computer has been succesfully destroyed!");
+    alert("this has been braught to you by Bin Laden");
+
+}
+
+promp ('what is your name biotch?!');
+    const name = prompt ('what is your name biotch?!');
+    alert (name);
+
+
+// 1. Display an alert dialog with the content: "Warning! This message will self-destruct in"
+
